d0de29bca9dd3348b92cc25cbe0fb65b1d04d78b
pipe2442 <felipeangulo2442@gmail.com>
2021-06-08 16:32:10 -0500

    Add images for project design

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-alert("chao bambino");
+(()=>{"use strict";document.getElementById("content").innerHTML+="<h1>Im the home</h1>",document.getElementById("content").innerHTML+="<h4>the other inner</h4>",document.getElementById("content").innerHTML+='<img src="./assets/images/onepiece.jpg" alt="">'})();
