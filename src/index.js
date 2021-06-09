5d2a3d3fd57839d9ec35faa3aaad37a72c5019c0
pipe2442 <felipeangulo2442@gmail.com>
2021-06-09 16:29:51 -0500

    Create navbar with correct styling and javascript loaded with webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import homefile from './modules/home';
 
-homefile();
+function nav() {
+    document.getElementById('content').innerHTML += 
+    '<nav>'+
+        '<ul>'+
+            '<li><a href="#">Home</a></li>'+
+            '<li><a href="#">Menu</a></li>'+
+            '<li><a href="#">Contact</a></li>'+
+        '</ul>'+
+    '</nav>'
+};
+
+nav();
+
+//homefile();
--- src/modules/home.js
+++ src/modules/home.js
@@ -1,7 +1,7 @@
 function homefile() {
     document.getElementById('content').innerHTML += '<h1>Im the home</h1>';
     document.getElementById('content').innerHTML += '<h4>the other inner</h4>';
-    document.getElementById('content').innerHTML += '<img src="./assets/images/onepiece.jpg" alt="">';
+    document.getElementById('content').innerHTML += '<img src="./assets/images/onepiece.jpg" alt="main image" class="mainpic">';
 };
 
 export default homefile;
