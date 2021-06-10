3547afcce8fd4c719688d71b08f7820f22e35ca0
pipe2442 <felipeangulo2442@gmail.com>
2021-06-10 15:36:30 -0500

    Add and fix linters

--- src/index.js
+++ src/index.js
@@ -3,59 +3,55 @@ import menufile from './modules/menu';
 import contactfile from './modules/contact';
 
 function nav() {
-    const newDiv = document.createElement('div');
-    newDiv.innerHTML = 
-    '<nav>'+
-        '<ul>'+
-            '<button id="homeBtn">Home</button>'+
-            '<button id="menuBtn">Menu</button>'+
-            '<button id="contactBtn">Contact</button>'+
-        '</ul>'+
-    '</nav>'
-    document.getElementById('content').appendChild(newDiv);
+  const newDiv = document.createElement('div');
+  newDiv.innerHTML = '<nav>'
+        + '<ul>'
+            + '<button id="homeBtn">Home</button>'
+            + '<button id="menuBtn">Menu</button>'
+            + '<button id="contactBtn">Contact</button>'
+        + '</ul>'
+    + '</nav>';
+  document.getElementById('content').appendChild(newDiv);
 
-    document.getElementById("homeBtn").onclick = showHome;
-    document.getElementById("menuBtn").onclick = showMenu;
-    document.getElementById("contactBtn").onclick = showContact;
-      
-    function showHome() {  
-        homefile()  
-    }
-    function showMenu() { 
-        menufile()  
-    }
-    function showContact() { 
-        contactfile()
-    }
-};
+  document.getElementById('homeBtn').onclick = showHome;
+  document.getElementById('menuBtn').onclick = showMenu;
+  document.getElementById('contactBtn').onclick = showContact;
 
+  function showHome() {
+    homefile();
+  }
+  function showMenu() {
+    menufile();
+  }
+  function showContact() {
+    contactfile();
+  }
+}
 
-document.getElementById('content').innerHTML = ""
+document.getElementById('content').innerHTML = '';
 nav();
 const homeDiv = document.createElement('div');
-homeDiv.innerHTML += 
-'<div class="mainpage">'+
-'<h1>ASIA DRAGON</h1>'+
-'</div>'+
-'<div class="description">'+
-    '<img src="./assets/images/salmon.jpg" alt="salmon">'+
-    '<div class="innerText">'+
-        '<h1>ABOUT US</h1>'+
-        '<p>'+
-            'ASIA DRAGON made its debut on Sunset Blvd in Los Angeles in 1983, introducing dim sum and other'+ 
-            'Chinese favorites prepared using traditional Cantonese cooking techniques. Each dish is made with'+ 
-            'premium ingredients, fresh-to-order, without adding any MSG, and served in a contemporary environment'+ 
-            'with friendly and efficient service.'+
-        '</p>'+
-    '</div>'+
-'</div>'
+homeDiv.innerHTML
++= '<div class="mainpage">'
++ '<h1>ASIA DRAGON</h1>'
++ '</div>'
++ '<div class="description">'
+    + '<img src="./assets/images/salmon.jpg" alt="salmon">'
+    + '<div class="innerText">'
+        + '<h1>ABOUT US</h1>'
+        + '<p>'
+            + 'ASIA DRAGON made its debut on Sunset Blvd in Los Angeles in 1983, introducing dim sum and other'
+            + 'Chinese favorites prepared using traditional Cantonese cooking techniques. Each dish is made with'
+            + 'premium ingredients, fresh-to-order, without adding any MSG, and served in a contemporary environment'
+            + 'with friendly and efficient service.'
+        + '</p>'
+    + '</div>'
++ '</div>';
 document.getElementById('content').appendChild(homeDiv);
 const footDiv = document.createElement('div');
-footDiv.innerHTML = 
-'<div class="footer">'+
-    '<p>© Copyright Luis Felipe Angulo Torres</p>'+
-'</div>'
+footDiv.innerHTML = '<div class="footer">'
+    + '<p>© Copyright Luis Felipe Angulo Torres</p>'
++ '</div>';
 document.getElementById('content').appendChild(footDiv);
 
-
-export default nav
+export default nav;
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,79 +1,78 @@
-import nav from './../index'
+import nav from '../index';
 
 function menufile() {
-    document.getElementById('content').innerHTML = ""
-    nav();
-    const menuDiv = document.createElement('div');
-    menuDiv.innerHTML += 
-    '<div class="menu">'+
-        '<div class="dish">'+ 
-            '<img src="https://images.rappi.com/products/2091744430-1593818637627.jpg?d=400x400" alt="monster">'+ 
-            '<div class="dishDescription">'+ 
-                '<h1>Monster Roll</h1>'+ 
-                '<p>'+ 
-                    'Tempura white fish sushi and crumbled crab palm with our special Teriyaki.'+ 
-                '</p>'+ 
-            '</div>'+ 
-       ' </div>'+ 
+  document.getElementById('content').innerHTML = '';
+  nav();
+  const menuDiv = document.createElement('div');
+  menuDiv.innerHTML
+    += '<div class="menu">'
+        + '<div class="dish">'
+            + '<img src="https://images.rappi.com/products/2091744430-1593818637627.jpg?d=400x400" alt="monster">'
+            + '<div class="dishDescription">'
+                + '<h1>Monster Roll</h1>'
+                + '<p>'
+                    + 'Tempura white fish sushi and crumbled crab palm with our special Teriyaki.'
+                + '</p>'
+            + '</div>'
+       + ' </div>'
 
-        '<div class="dish">'+ 
-            '<img src="https://images.rappi.com/products/2091744437-1593818719473.jpg?d=400x400" alt="Yasai Tempura Roll">'+ 
-            '<div class="dishDescription">'+ 
-                '<h1>Yasai Tempura Roll </h1>'+ 
-                '<p>'+ 
-                    'Vegetable sushi in tempura katsura, classic avocado and special Teriyaki.'+ 
-                '</p>'+ 
-            '</div>'+ 
-        '</div>'+ 
+        + '<div class="dish">'
+            + '<img src="https://images.rappi.com/products/2091744437-1593818719473.jpg?d=400x400" alt="Yasai Tempura Roll">'
+            + '<div class="dishDescription">'
+                + '<h1>Yasai Tempura Roll </h1>'
+                + '<p>'
+                    + 'Vegetable sushi in tempura katsura, classic avocado and special Teriyaki.'
+                + '</p>'
+            + '</div>'
+        + '</div>'
 
-        '<div class="dish">'+ 
-            '<img src="https://images.rappi.com/products/2091744431-1593818510075.jpg?d=400x400" alt="Ceviche Roll ">'+ 
-            '<div class="dishDescription">'+ 
-                '<h1>Ceviche Roll </h1>'+ 
-                '<p>'+ 
-                    'Peruvian style fish sushi in tiger milk, avocado and shrimp.'+  
-                '</p>'+ 
-            '</div>'+ 
-        '</div>'+ 
+        + '<div class="dish">'
+            + '<img src="https://images.rappi.com/products/2091744431-1593818510075.jpg?d=400x400" alt="Ceviche Roll ">'
+            + '<div class="dishDescription">'
+                + '<h1>Ceviche Roll </h1>'
+                + '<p>'
+                    + 'Peruvian style fish sushi in tiger milk, avocado and shrimp.'
+                + '</p>'
+            + '</div>'
+        + '</div>'
 
-        '<div class="dish">'+ 
-            '<img src="https://images.rappi.com/products/2090645015-1563035613.png?d=128x90" alt="Tiger Eye Roll ">'+ 
-            '<div class="dishDescription">'+ 
-                '<h1>Tiger Eye Roll </h1>'+ 
-                '<p>'+ 
-                    'Tempura sushi with tuna, salmon, crab palm, avocado and sesame in dynamite sauce, 6 Bites.'+  
-                '</p>'+ 
-            '</div>'+ 
-        '</div>'+ 
+        + '<div class="dish">'
+            + '<img src="https://images.rappi.com/products/2090645015-1563035613.png?d=128x90" alt="Tiger Eye Roll ">'
+            + '<div class="dishDescription">'
+                + '<h1>Tiger Eye Roll </h1>'
+                + '<p>'
+                    + 'Tempura sushi with tuna, salmon, crab palm, avocado and sesame in dynamite sauce, 6 Bites.'
+                + '</p>'
+            + '</div>'
+        + '</div>'
 
-        '<div class="dish">'+ 
-            '<img src="https://images.rappi.com/products/2090644962-1563037158.png?d=400x400" alt="Spicy Tuna Maki">'+ 
-            '<div class="dishDescription">'+ 
-                '<h1>Spicy Tuna Maki</h1>'+ 
-                '<p>'+ 
-                    'Yellow fin tuna sushi, avocado and dynamite sauce 9 pieces.'+ 
-                '</p>'+ 
-            '</div>'+ 
-        '</div>'+ 
+        + '<div class="dish">'
+            + '<img src="https://images.rappi.com/products/2090644962-1563037158.png?d=400x400" alt="Spicy Tuna Maki">'
+            + '<div class="dishDescription">'
+                + '<h1>Spicy Tuna Maki</h1>'
+                + '<p>'
+                    + 'Yellow fin tuna sushi, avocado and dynamite sauce 9 pieces.'
+                + '</p>'
+            + '</div>'
+        + '</div>'
 
-        '<div class="dish">'+ 
-            '<img src="https://images.rappi.com/products/2090645018-1563217418.png?d=400x400" alt="Teriyaki Roll">'+ 
-            '<div class="dishDescription">'+ 
-                '<h1>Teriyaki Roll</h1>'+ 
-                '<p>'+ 
-                    'Teriyaki sushi philadelphia tempura, in teriyaki sauce 9 bites.'+ 
-                '</p>'+ 
-            '</div>'+ 
-        '</div>'+ 
+        + '<div class="dish">'
+            + '<img src="https://images.rappi.com/products/2090645018-1563217418.png?d=400x400" alt="Teriyaki Roll">'
+            + '<div class="dishDescription">'
+                + '<h1>Teriyaki Roll</h1>'
+                + '<p>'
+                    + 'Teriyaki sushi philadelphia tempura, in teriyaki sauce 9 bites.'
+                + '</p>'
+            + '</div>'
+        + '</div>'
 
-    '</div>' 
-    document.getElementById('content').appendChild(menuDiv);
-    const footDiv = document.createElement('div');
-    footDiv.innerHTML = 
-    '<div class="footer">'+
-        '<p>© Copyright Luis Felipe Angulo Torres</p>'+
-    '</div>'
-    document.getElementById('content').appendChild(footDiv);
-};
+    + '</div>';
+  document.getElementById('content').appendChild(menuDiv);
+  const footDiv = document.createElement('div');
+  footDiv.innerHTML = '<div class="footer">'
+        + '<p>© Copyright Luis Felipe Angulo Torres</p>'
+    + '</div>';
+  document.getElementById('content').appendChild(footDiv);
+}
 
 export default menufile;
